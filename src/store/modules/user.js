bad7f31ba0bf50a42fac4bdbe7d463da6f6f5a48
duu233 <494303210qq.com>
2024-12-16 09:41:57 +0800

    联调登录

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -6,14 +6,8 @@ import {
   getKeyValues,
   getChildAppCodes
 } from '@/api/login'
-import {
-  Message,
-} from 'element-ui'
-import {
-  getToken,
-  setToken,
-  removeToken
-} from '@/utils/auth'
+import { Message } from 'element-ui'
+import { getToken, setToken, removeToken } from '@/utils/auth'
 
 const user = {
   state: {
@@ -21,7 +15,7 @@ const user = {
     name: '',
     avatar: '',
     roles: [],
-    permissions: [],
+    permissions: []
   },
 
   mutations: {
@@ -38,7 +32,7 @@ const user = {
       state.roles = roles
     },
     SET_PERMISSIONS: (state, permissions) => {
-      state.permissions = permissions;
+      state.permissions = permissions
       // permissions.forEach((v, i) => {
       //   if (state.permissions.includes(v)) return
       //   state.permissions.push(v);
@@ -48,60 +42,61 @@ const user = {
 
   actions: {
     // 登录
-    Login({
-      commit
-    }, userInfo) {
+    Login({ commit }, userInfo) {
       const username = userInfo.username.trim()
       return new Promise((resolve, reject) => {
-        login(username, userInfo.password).then(response => {
-          // const data = response.data
-          // const tokenStr = data.tokenHead+data.token
-          const data = response.retData
-          const tokenStr = data.adminToken
-          setToken(tokenStr)
-          commit('SET_TOKEN', tokenStr)
-          resolve(response)
-        }).catch(error => {
-          reject(error)
-        })
+        login(username, userInfo.password)
+          .then(response => {
+            // const data = response.data
+            // const tokenStr = data.tokenHead+data.token
+            const data = response.retData
+
+            const tokenStr = data.adminToken
+            setToken(tokenStr)
+            commit('SET_TOKEN', tokenStr)
+            resolve(response)
+          })
+          .catch(error => {
+            reject(error)
+          })
       })
     },
-    // 获取顶部菜单栏数据   
-    GetSysMenus({
-      commit,
-      state
-    }, params) {
+    // 获取顶部菜单栏数据
+    GetSysMenus({ commit, state }, params) {
       return new Promise((resolve, reject) => {
-        getSysMenus(params).then(response => {
-          // 获取通用字典
-          getKeyValues().then((res) => {
-            if (res.retCode === 200) {
-              sessionStorage.setItem('allKeyValues', JSON.stringify(res.retData));
-            }
+        getSysMenus(params)
+          .then(response => {
+            // 获取通用字典
+            getKeyValues().then(res => {
+              if (res.retCode === 200) {
+                sessionStorage.setItem(
+                  'allKeyValues',
+                  JSON.stringify(res.retData)
+                )
+              }
+            })
+            getChildAppCodes().then(res => {
+              if (res.retCode === 200) {
+                commit('SET_PERMISSIONS', res.retData)
+                resolve(response)
+              } else {
+                Message({
+                  type: 'error',
+                  message: '获取用户权限失败',
+                  duration: 2000
+                })
+                resolve(response)
+              }
+            })
           })
-          getChildAppCodes().then((res) => {
-            if (res.retCode === 200) {
-              commit('SET_PERMISSIONS', res.retData);
-              resolve(response)
-            } else {
-              Message({
-                type: "error",
-                message: "获取用户权限失败",
-                duration: 2000
-              });
-              resolve(response)
-            }
+          .catch(error => {
+            reject(error)
           })
-
-
-        }).catch(error => {
-          reject(error)
-        })
       })
     },
     // 获取用户信息
     // GetInfo({
-    //   commit, 
+    //   commit,
     //   state
     // }, id) {
     //   return new Promise((resolve, reject) => {
@@ -122,12 +117,8 @@ const user = {
     // },
 
     // 登出
-    LogOut({
-      commit,
-      state
-    }) {
+    LogOut({ commit, state }) {
       return new Promise((resolve, reject) => {
-
         commit('SET_TOKEN', '')
         commit('SET_ROLES', [])
         removeToken()
@@ -144,9 +135,7 @@ const user = {
     },
 
     // 前端 登出
-    FedLogOut({
-      commit
-    }) {
+    FedLogOut({ commit }) {
       return new Promise(resolve => {
         commit('SET_TOKEN', '')
         removeToken()
--- src/utils/requestPay.js
+++ src/utils/requestPay.js
@@ -1,14 +1,8 @@
 import axios from 'axios'
-import {
-  Message,
-  MessageBox,
-
-} from 'element-ui'
+import { Message, MessageBox } from 'element-ui'
 import store from '../store'
-import {
-  getToken
-} from '@/utils/auth'
-import md5 from "js-md5";
+import { getToken } from '@/utils/auth'
+import md5 from 'js-md5'
 // 创建axios实例
 
 const service = axios.create({
@@ -16,73 +10,76 @@ const service = axios.create({
   timeout: 15000 // 请求超时时间
 })
 
-
-
-service.interceptors.request.use(config => {
-  //前端 通过时间 判断token是否过期
-  let tokenTime = sessionStorage.getItem('tokenTime');
-  let newTime = new Date().getTime();
-  if (!tokenTime) {
-    sessionStorage.setItem('tokenTime', newTime)
-  } else {
-    if (newTime - tokenTime >= 1800000 && config.url.indexOf('login') === -1) {
-      store.dispatch('LogOut').then(() => {
-        location.reload() // 为了重新实例化vue-router对象 避免bug
-      })
-      sessionStorage.removeItem('tokenTime')
-    } else {
+service.interceptors.request.use(
+  config => {
+    //前端 通过时间 判断token是否过期
+    let tokenTime = sessionStorage.getItem('tokenTime')
+    let newTime = new Date().getTime()
+    if (!tokenTime) {
       sessionStorage.setItem('tokenTime', newTime)
+    } else {
+      if (
+        newTime - tokenTime >= 1800000 &&
+        config.url.indexOf('login') === -1
+      ) {
+        store.dispatch('LogOut').then(() => {
+          location.reload() // 为了重新实例化vue-router对象 避免bug
+        })
+        sessionStorage.removeItem('tokenTime')
+      } else {
+        sessionStorage.setItem('tokenTime', newTime)
+      }
     }
-  }
-  //签名-------------s
-  let randomString = new Date().toLocaleDateString().split("/").join("") +
-    Math.floor(Math.random() * 10).toString();
-  if (randomString.length === 7) {
-    randomString = randomString + Math.floor(Math.random() * 10).toString();
-  }
-  let sign = randomString + "8e808087-08b3-3e10-8e83-93bf078df4b2";
-  sign = md5(sign)
-  //签名-------------e
-  if (config.method === 'get') {
-    if (!config.params) {
-      config.params = {};
+    //签名-------------s
+    let randomString =
+      new Date()
+        .toLocaleDateString()
+        .split('/')
+        .join('') + Math.floor(Math.random() * 10).toString()
+    if (randomString.length === 7) {
+      randomString = randomString + Math.floor(Math.random() * 10).toString()
     }
-    config.params.randomString = randomString;
-    config.params.sign = sign;
-    
-    if (getToken()) {
-      config.params.adminToken = getToken();
+    let sign = randomString + '8e808087-08b3-3e10-8e83-93bf078df4b2'
+    sign = md5(sign)
+    //签名-------------e
+    if (config.method === 'get') {
+      if (!config.params) {
+        config.params = {}
+      }
+      config.params.randomString = randomString
+      config.params.sign = sign
+
+      if (getToken()) {
+        config.params.userToken = getToken()
+      }
     }
-  }
-  if (config.method === 'post') {
- 
-    if (!config.data) {
-      config.data = {};
-    }
-    config.data.randomString = randomString;
-    config.data.sign = sign;
-    if (getToken()) {
-      config.data.adminToken = getToken();
+    if (config.method === 'post') {
+      if (!config.data) {
+        config.data = {}
+      }
+      config.data.randomString = randomString
+      config.data.sign = sign
+      if (getToken()) {
+        config.data.userToken = getToken()
+      }
     }
 
+    // if (store.getters.token) {
+    //   config.headers['Authorization'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+    // }
+
+    return config
+  },
+  error => {
+    // Do something with request error
+    console.log(error) // for debug
+    Promise.reject(error)
   }
-
-  // if (store.getters.token) {
-  //   config.headers['Authorization'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
-  // }
-
-
-  return config
-}, error => {
-  // Do something with request error
-  console.log(error) // for debug
-  Promise.reject(error)
-})
+)
 
 // respone拦截器
 service.interceptors.response.use(
   response => {
-
     /**
      * code为非200是抛错 可结合自己业务进行修改
      */
@@ -107,11 +104,15 @@ service.interceptors.response.use(
       //   })
       // }
       if (res.retCode === 406) {
-        MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
-          confirmButtonText: '重新登录',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
+        MessageBox.confirm(
+          '你已被登出，可以取消继续留在该页面，或者重新登录',
+          '确定登出',
+          {
+            confirmButtonText: '重新登录',
+            cancelButtonText: '取消',
+            type: 'warning'
+          }
+        ).then(() => {
           store.dispatch('FedLogOut').then(() => {
             location.reload() // 为了重新实例化vue-router对象 避免bug
           })
@@ -119,8 +120,6 @@ service.interceptors.response.use(
       }
       return Promise.reject('error')
     } else {
-
-
       return response.data
     }
   },
